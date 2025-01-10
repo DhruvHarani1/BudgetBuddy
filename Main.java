849f40cc6dc79c05f543716e82cfd824190082a2
DhruvHarani1 <dhruvharani5@gmail.com>
2025-01-10 20:23:21 +0530

    security question and Pin

--- Main.java
+++ Main.java
@@ -57,37 +57,95 @@ class SignUp {
     String firstName;
     String lastName;
     String mobileNumber;
+    String PassKey;
+    String SecurityQuestionDisplay;
+    String SecurityAnswer;
 
     // classes
     Scanner sc = new Scanner(System.in);
 
     void getDetails() {
-        boolean mobileNum; // to validate Mobile Number...
+        boolean flag; // to validate Mobile Number...
 
         System.out.print("Enter First Name: ");
         firstName = sc.nextLine();
         System.out.print("Enter Last Name: ");
         lastName = sc.nextLine();
 
+        // loop for mobile number...
         do {
-
-            mobileNum = true;
-
+            flag = true;
             System.out.print("Enter Mobile Number: ");
             mobileNumber = sc.nextLine();
 
             if (mobileNumber.length() != 10) { // to validate length
-                mobileNum = false;
+                flag = false;
                 System.out.println("Number should be of 10 digit!!!\n");
             }
-            for (int i = 0; i < mobileNumber.length(); i++) { // to validate integers
+            for (int i = 0; i < mobileNumber.length(); i++) { // to validate number is an integers
                 if (mobileNumber.charAt(i) < '0' || mobileNumber.charAt(i) > '9') {
-                    mobileNum = false;
+                    flag = false;
                     System.out.println("Number Should Not contain Alpabetical Characters!!!\n");
                     break;
                 }
             }
-        } while (!mobileNum);
+        } while (!flag);
+
+        // loop for Pin...
+        do {
+            flag = true;
+            System.out.print("Enter 4 Digit Security Pin: ");
+            PassKey = sc.nextLine();
+
+            if (PassKey.length() != 4) {
+                flag = false;
+                System.out.println("Pin should be of 4 digit!!!\n");
+            }
+
+            for (int i = 0; i < PassKey.length(); i++) { // to validate Pin is an integer
+                if (PassKey.charAt(i) < '0' || PassKey.charAt(i) > '9') {
+                    flag = false;
+                    System.out.println("Pin Should Not contain Alpabetical Characters!!!\n");
+                    break;
+                }
+            }
+        } while (!flag);
+
+        //securtity question
+        System.out.println("Worried! Of Forgetting Pin");
+        System.out.println("Just SetUp a Security Question And Enjoy\n");
+        System.out.println("Press 1 for ---> Who is Your Favourite Teacher?");
+        System.out.println("Press 2 for ---> What is Your Favourite Food?");
+        System.out.println("Press 3 for ---> Which is Your Favourite Pet?");
+
+        int QuestionNumber = sc.nextInt();
+        sc.nextLine();
+
+        switch (QuestionNumber) {
+            case 1:
+                SecurityQuestionDisplay = "Who is Your Favourite Teacher?";
+                System.out.print("Enter His Name: ");
+                SecurityAnswer = sc.nextLine();
+                break;
+            case 2:
+                SecurityQuestionDisplay = "What is Your Favourite Food?";
+                System.out.print("Enter Name Of Fruit : ");
+                SecurityAnswer = sc.nextLine();
+                System.out.println("\nSound's Delicious!\n");
+                break;
+            case 3:
+                SecurityQuestionDisplay = "Which is Your Favourite Pet?";
+                System.out.print("Enter Name Of Animal : ");
+                SecurityAnswer = sc.nextLine();
+                System.out.println("\nAwesome! You've chosen the best animal on the planet!\n");
+                break;
+            default:
+                System.out.println("\nERROR Press from 1 to 3");
+                sc.nextLine();
+                System.out.print("\nPress Enter To Continue...");
+                sc.nextLine();
+                break;
+        }
 
     }
 
